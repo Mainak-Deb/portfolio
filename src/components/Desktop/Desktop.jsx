b43e42a10be00536527be668b066d1d77ed004e6
Mainak Deb <debmainak37@gmail.com>
2023-09-24 23:51:33 +0530

    dragable added

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Desktop.css'
 import Doc from '../Doc/Doc'
+import { format } from 'date-fns';
+import DraggableResizable from '../DraggableResizable/DraggableResizable';
 
 const Desktop = () => {
+
+   const [time, settime] = useState(new Date())
+
+   useEffect(()=>{
+      setInterval(() => {
+        settime(new Date())
+      }, 1000);
+   })
+
   return (
     <div id='desktop-body'  > 
+   <DraggableResizable/>
     <div className="top">
-      
+      <span>{format(time, 'dd MMM yyyy')}</span>
+      <span>{format(time, 'HH:mm:ss')}</span>
+      <span>{format(time, 'EEEE')}</span>
     </div>   
       <Doc></Doc>
     </div>
