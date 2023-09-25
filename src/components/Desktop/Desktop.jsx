9c8bc2f7ec5ca7fc1a84b623368cceaf559254e5
Mainak Deb <debmainak37@gmail.com>
2023-09-25 23:45:13 +0530

    starfield added

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useRef } from 'react'
 import './Desktop.css'
 import Doc from '../Doc/Doc'
 import { format } from 'date-fns';
@@ -9,17 +9,21 @@ import Links from '../Windows/Links/Links';
 import Profile from '../Windows/Profile/Profile';
 import Msg from '../Windows/Msg/Msg';
 
-const Desktop = () => {
+var Desktop = () => {
 
-   const [time, settime] = useState(new Date())
+   var [time, settime] = useState(new Date())
 
 
-   const [profileOpen, setprofileOpen] = useState(false);
-   const [jobOpen, setjobOpen] = useState(false);
-   const [devOpen, setdevOpen] = useState(false);
-   const [skillOpen, setskillOpen] = useState(false);
-   const [msgOpen, setmsgOpen] = useState(false);
-   const [linksOpen, setlinksOpen] = useState(false);
+   var [profileOpen, setprofileOpen] = useState(false);
+   var [jobOpen, setjobOpen] = useState(false);
+   var [devOpen, setdevOpen] = useState(false);
+   var [skillOpen, setskillOpen] = useState(false);
+   var [msgOpen, setmsgOpen] = useState(false);
+   var [linksOpen, setlinksOpen] = useState(false);
+
+
+   var canvasRef = useRef(null);
+  var stars = [];
 
    function closeAll(){
       setprofileOpen(false);
@@ -52,15 +56,113 @@ const Desktop = () => {
    }
 
 
-   useEffect(()=>{
-      setInterval(() => {
-        settime(new Date())
-      }, 1000);
-   })
+   useEffect(() => {
+
+    setInterval(() => {
+      settime(new Date())
+    }, 100);
+    var canvas = canvasRef.current;
+    var ctx = canvas.getContext('2d');
+    var numStars = 400;
+    var stars=[];
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    var resizeCanvas = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      stars=[]
+      for (let i = 0; i < numStars; i++) {
+        var x = Math.random() * canvas.width ;
+        var y = Math.random() * canvas.height ;
+        var z = Math.random() * canvas.width ;
+        
+        var angle = calculateAngle(x,y,canvas.width/2,canvas.width/2);
+        var radius = 1
+        var d =0;
+        var dd = Math.random() *10;
+        stars.push({ x, y, z, radius,angle,d,dd });
+      }
+      update()
+    };
+
+    function calculateAngle(x1, y1, x2, y2) {
+      return Math.atan2(y2 - y1, x2 - x1);      
+    }
+
+    // Initialize canvas size
+    resizeCanvas();
+
+    for (let i = 0; i < numStars; i++) {
+      var x = Math.random() * canvas.width;
+      var y = Math.random() * canvas.height;
+      var z = Math.random() * canvas.width;
+      
+      var angle = calculateAngle(x,y,canvas.width/2,canvas.height/2);
+      var radius = 1
+      var d =0;
+      var dd = Math.random() *10;
+      stars.push({ x, y, z, radius,angle,d,dd });
+    }
+
+    // Update and draw the starfield with depth
+    function update() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      for (let i = 0; i < numStars; i++) {
+        var star = stars[i];
+        star.z -= 1; // Adjust the speed of movement towards the viewer
+        if (star.d>width/2) {
+          star.x = Math.random() * width;
+          star.y = Math.random() * height;
+          star.z = Math.random() * width;
+          
+          star.angle = calculateAngle(star.x,star.y,width/2,height/2);
+          star.radius = 1
+          star.d =0;
+          star.dd = Math.random() *10;
+        }
+
+        var scale = width / (star.z + 1); // Adjust the scale based on depth
+        var x = star.x-(star.d*Math.cos(star.angle))
+        var y = star.y-(star.d*Math.sin(star.angle))
+        star.d+=star.dd;
+        var radius = star.radius+(star.d/width)*4;
+
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.fill();
+      }
+
+      requestAnimationFrame(update);
+    }
+
+    update();
+
+    // Handle window resize
+    window.addEventListener('resize', resizeCanvas);
+
+    // Cleanup
+    return () => {
+      cancelAnimationFrame(update);
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, []);
 
   return (
     <div id='desktop-body'  > 
-    
+    <div className="desktop_text">
+    <canvas  id="canvas" ref={canvasRef} />;
+      <div>
+        <h1>Mainak Deb</h1>
+        <p>Software Engineer</p>
+      </div>
+      
+    </div>
+
+
 
 
     <div className="top">
